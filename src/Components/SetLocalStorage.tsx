e3131b1f1eb19025ac538c85d810f9bab4ea8aa9
Bogdan <bogdan_mykhailov@icloud.com>
2022-03-07 11:34:03 +0200

    add localstorage

--- src/Components/SetLocalStorage.tsx
+++ src/Components/SetLocalStorage.tsx
@@ -6,8 +6,6 @@ type SetLocalStorageHandlerPropsType = {
 
 export const SetLocalStorage = (props: SetLocalStorageHandlerPropsType) => {
 
-
-
   const SetLocalStorageHandler = () => {
     localStorage.setItem('counterKey', JSON.stringify(props.counter))
     localStorage.setItem('counterKey + 1', JSON.stringify(props.counter + 1))
